ca75e12c7ecae8178a0b28e58b7535f5f53a8480
Steve Akinyemi <steveakinyemi@gmail.com>
2015-09-09 07:41:20 +0100

    Added ScripParser class. This class will parse the the script file and it is going to be part of a larger class eventually.

--- src/com/simplescript/Main.java
+++ src/com/simplescript/Main.java
@@ -2,7 +2,8 @@ package com.simplescript;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+	    // TODO
     }
 }
---
+++ src/com/simplescript/ScriptParser.java
@@ -0,0 +1,8 @@
+package com.simplescript;
+
+/**
+ * Created by Nypro_PC on 09/09/2015.
+ */
+public class ScriptParser
+{
+}
